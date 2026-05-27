03e3e804ecd11361612563018a35938cc9e4702b
agusherrera478 <agustinaherrera478@gmail.com>
2026-05-27 01:44:59 -0300

    clase material

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,14 +1,13 @@
 
-
 class Material(object):
 
-        def __init__(self, titulo, autor, editorial):
+    def __init__(self, titulo, autor, editorial):
         self.titulo = titulo
         self.autor = autor
         self.editorial = editorial
         self.disponible = True
 
    
-        def _str_(self):
-        return f"{self.titulo} - {self.autor}"
+    def _str_(self):
+        return f"Titulo: {self.titulo}, Autor: {self.autor}, Editorial: {self.editorial}"
 
