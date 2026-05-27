e5b9242e52864f83d5af09ea4e6882b822182d59
agusherrera478 <agustinaherrera478@gmail.com>
2026-05-27 01:17:56 -0300

    actualización de cambios

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,8 +1,14 @@
-class Material(ABC):
 
-    def __init__(self, titulo, autor, editorial):
+
+class Material(object):
+
+        def __init__(self, titulo, autor, editorial):
         self.titulo = titulo
         self.autor = autor
         self.editorial = editorial
         self.disponible = True
+
+   
+        def _str_(self):
+        return f"{self.titulo} - {self.autor}"
 
