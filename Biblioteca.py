75f46ab1fa95bc913cef0fd1c372359346d3795f
agusherrera478 <agustinaherrera478@gmail.com>
2026-06-05 22:01:39 -0300

    Actualizacion de clases

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,12 +1,32 @@
+from abc import ABC
+
 class Material(object):
 
-    def __init__(self, titulo, autor, editorial):
+    def __init__(self, titulo, editorial, ano, disponible):
         self.titulo = titulo
-        self.autor = autor
         self.editorial = editorial
-        self.disponible = True
+        self.ano = ano 
+        self.disponible = disponible
 
    
-    def _str_(self):
-        return f"Titulo: {self.titulo}, Autor: {self.autor}, Editorial: {self.editorial}"
-
+    def __str__(self):
+        return f"Titulo: {self.titulo}, Editorial: {self.editorial}, Ano: {self.ano}, {self.disponible}"
+        
+        
+class Libro(Material):
+    def __init__(self, titulo, editorial, ano, disponible, autor, genero, id_libro):
+        super().__init__(titulo, editorial, ano, disponible)
+        self.autor = autor
+        self.genero = genero
+        self.id = id_libro
+        
+class Revista(Material):
+    def __init__(self, titulo, editorial, ano, disponible, numero, fecha):
+        super().__init__(titulo, editorial, ano, disponible)
+        self.numero = numero
+        self.fecha = fecha
+        
+        
+a = Libro("La sirenita","Pececitos", "1999", True, "Cosme fulanito", "infantil", "465468468")
+print(a)
+        
